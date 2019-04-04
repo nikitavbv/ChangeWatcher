65c9852ca93093412d11d68a5ea1d1633a82eaa8
Nikita Volobuev <nikitavbv@gmail.com>
2019-04-04 22:36:17 +0300

    feat(javadoc): add javadoc for RouteConstants

--- src/main/java/com/nikitavbv/changewatcher/RouteConstants.java
+++ src/main/java/com/nikitavbv/changewatcher/RouteConstants.java
@@ -7,13 +7,20 @@ package com.nikitavbv.changewatcher;
  */
 public class RouteConstants {
 
+  /** Prefix for all API routes. */
   public static final String API_ROOT = "/api/v1";
+  /** Pattern for API routes. */
   public static final String API_PATH_PATTERN = API_ROOT + "/**";
 
+  /** Session init API URL. */
   public static final String INIT_API = API_ROOT + "/init";
+  /** Authentication API URL. */
   public static final String LOGIN_API = API_ROOT + "/login";
+  /** User data API URL. */
   public static final String USERS_API = API_ROOT + "/users";
+  /** Webpage preview API URL. */
   public static final String PREVIEW_API = API_ROOT + "/preview";
+  /** Watching job API URL. */
   public static final String JOBS_API = API_ROOT + "/jobs";
 
 }
