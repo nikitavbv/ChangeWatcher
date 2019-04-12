74fe87630099dab3eb4b7f2ecda6ad2a52bf4af3
Nikita Volobuev <nikitavbv@gmail.com>
2019-04-12 20:00:26 +0300

    feat(ErrorResponse): add javadoc

--- src/main/java/com/nikitavbv/changewatcher/api/ErrorResponse.java
+++ src/main/java/com/nikitavbv/changewatcher/api/ErrorResponse.java
@@ -7,22 +7,28 @@ package com.nikitavbv.changewatcher.api;
  */
 public class ErrorResponse {
 
+  /** Error name. */
   private String error;
+  /** Error message. */
   private String message;
 
+  /** Construct ErrorResponse given error name and message. */
   public ErrorResponse(String error, String message) {
     this.error = error;
     this.message = message;
   }
 
+  /** Construct ErrorResponse given error name. */
   public ErrorResponse(String error) {
     this.error = error;
   }
 
+  /** Returns error name. */
   public String getError() {
     return error;
   }
 
+  /** Returns error message. */
   public String getMessage() {
     return message;
   }
