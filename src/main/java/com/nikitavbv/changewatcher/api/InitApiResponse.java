8e0de153352d4fb737991995d72788f887afd549
Nikita Volobuev <nikitavbv@gmail.com>
2019-04-15 21:48:46 +0300

    feat(InitApiResponse): add javadoc

--- src/main/java/com/nikitavbv/changewatcher/api/InitApiResponse.java
+++ src/main/java/com/nikitavbv/changewatcher/api/InitApiResponse.java
@@ -11,12 +11,19 @@ import java.util.List;
  */
 public class InitApiResponse {
 
+  /** List of user's watching jobs. */
   private List<WatchingJob> jobs;
 
+  /**
+   * Construct init api response.
+   *
+   * @param jobs list of watching jobs of user performing the request.
+   */
   public InitApiResponse(List<WatchingJob> jobs) {
     this.jobs = jobs;
   }
 
+  /** Return list of watching jobs. */
   public List<WatchingJob> getJobs() {
     return jobs;
   }
