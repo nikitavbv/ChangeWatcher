cd0bf83ca635f943fccc3c1b20be7c66e8d36485
Nikita Volobuev <nikitavbv@gmail.com>
2019-04-17 18:41:58 +0300

    feat(AddWatchingJobResponse): add javadoc

--- src/main/java/com/nikitavbv/changewatcher/jobs/AddWatchingJobResponse.java
+++ src/main/java/com/nikitavbv/changewatcher/jobs/AddWatchingJobResponse.java
@@ -9,18 +9,23 @@ import java.util.List;
  */
 public class AddWatchingJobResponse {
 
+  /** List of all watching jobs of this user. */
   private List<WatchingJob> jobs;
+  /** The ID of new job which was just created. */
   private long newJobID;
 
+  /** Construct add watching job response. */
   public AddWatchingJobResponse(long newJobID, List<WatchingJob> jobs) {
     this.jobs = jobs;
     this.newJobID = newJobID;
   }
 
+  /** Returns all watching jobs on this account. */
   public List<WatchingJob> getJobs() {
     return jobs;
   }
 
+  /** Returns id of newly created job. */
   public long getNewJobID() {
     return this.newJobID;
   }
