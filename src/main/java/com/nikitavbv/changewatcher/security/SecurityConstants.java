910ecc14544edd1acf24c9c07541ff31dde3f216
Nikita Volobuev <nikitavbv@gmail.com>
2019-07-14 11:16:48 +0300

    feat(SecurityConstants): add javadoc

--- src/main/java/com/nikitavbv/changewatcher/security/SecurityConstants.java
+++ src/main/java/com/nikitavbv/changewatcher/security/SecurityConstants.java
@@ -6,8 +6,12 @@ package com.nikitavbv.changewatcher.security;
  * @author Nikita Volobuev
  */
 public class SecurityConstants {
+  /** Auth token prefix as stored in HTTP auth header. */
   public static final String TOKEN_PREFIX = "Bearer ";
+  /** HTTP header for auth info. */
   public static final String HEADER_STRING = "Authorization";
+  /** Auth token TTL */
   public static final long EXPIRATION_TIME = 864_000_000; // 10 days
+  /** Length of application secret. */
   public static final int SECRET_SIZE = 512 / 8; // in bytes
 }
