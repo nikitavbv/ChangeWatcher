71590d655b1a3212bc943447481258b8de940a87
Nikita Volobuev <nikitavbv@gmail.com>
2019-07-29 09:33:45 +0300

    refactor(GeneratePreviewThread): reduce cyclomatic complexity

--- src/main/java/com/nikitavbv/changewatcher/preview/GeneratePreviewThread.java
+++ src/main/java/com/nikitavbv/changewatcher/preview/GeneratePreviewThread.java
@@ -1,18 +1,13 @@
 package com.nikitavbv.changewatcher.preview;
 
+import com.nikitavbv.changewatcher.ScreenshotThread;
 import java.io.File;
 import java.io.IOException;
-import java.util.HashMap;
 import java.util.Locale;
-import java.util.Map;
-import java.util.concurrent.TimeUnit;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.imageio.ImageIO;
-import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.firefox.GeckoDriverService;
 import ru.yandex.qatools.ashot.AShot;
 import ru.yandex.qatools.ashot.Screenshot;
 import ru.yandex.qatools.ashot.shooting.ShootingStrategies;
@@ -22,32 +17,10 @@ import ru.yandex.qatools.ashot.shooting.ShootingStrategies;
  *
  * @author Nikita Volobuev
  */
-public class GeneratePreviewThread extends Thread {
+public class GeneratePreviewThread extends ScreenshotThread {
 
   /** Logger for this class. */
   private static final Logger LOG = Logger.getLogger(GeneratePreviewThread.class.getName());
-
-  /** Browser window width when taking screenshot. */
-  private static final int WINDOW_WIDTH = 1366;
-  /** Browser window height when taking screenshot. */
-  private static final int WINDOW_HEIGHT = 768;
-  /** Image depth of virtual display where browser is launched. */
-  private static final int IMAGE_DEPTH = 24;
-  /** Screen mode of virtual display where browser is launched. */
-  private static final String SCREEN_MODE = WINDOW_WIDTH + "x" + WINDOW_HEIGHT + "x" + IMAGE_DEPTH;
-
-  /** Number of virtual display used for browser. */
-  private static final int DISPLAY_NUMBER = 1001;
-  /** Path to xvfb which is used to create virtual display. */
-  private static final String XVFB_PATH = "/usr/bin/Xvfb";
-  /** xvfb command to create virtual display. */
-  private static final String XVFB_COMMAND = XVFB_PATH
-          + " -br -nolisten tcp -screen 0 " + SCREEN_MODE + " :" + DISPLAY_NUMBER;
-  /** Path to geckodriver which is used to control firefox. */
-  private static final String GECKO_DRIVER_PATH = "/geckodriver/geckodriver";
-
-  /** Max number of seconds we wait before page is considered loaded. */
-  private static final int PAGE_LOAD_TIMEOUT = 10;
 
   /** Page url to make preview of. */
   private final String url;
@@ -74,39 +47,39 @@ public class GeneratePreviewThread extends Thread {
    * Run preview generation.
    */
   public void run() {
-    final Process xvfbProcess;
-    try {
-      xvfbProcess = Runtime.getRuntime().exec(XVFB_COMMAND);
-    } catch (IOException e) {
-      if (LOG.isLoggable(Level.WARNING)) {
-        LOG.log(Level.WARNING, "Failed to run xvfb process", e);
-      }
+    WebDriver driver = getWebDriver();
+    if (driver == null) {
       return;
     }
-    final Map<String, String> environment = new HashMap<>();
-    environment.put("DISPLAY", ":" + DISPLAY_NUMBER + ".0");
-    System.setProperty("webdriver.gecko.driver", GECKO_DRIVER_PATH);
-    final GeckoDriverService service = new GeckoDriverService.Builder()
-      .usingAnyFreePort()
-      .withEnvironment(environment)
-      .build();
-    final WebDriver driver = new FirefoxDriver(service);
-    driver.manage().timeouts().pageLoadTimeout(PAGE_LOAD_TIMEOUT, TimeUnit.SECONDS);
-    driver.manage().window().setSize(new Dimension(WINDOW_WIDTH, WINDOW_HEIGHT));
     driver.get(url);
     final Screenshot screenshot = new AShot()
       .shootingStrategy(ShootingStrategies.viewportPasting(1000))
       .takeScreenshot(driver);
 
+    try {
+      ImageIO.write(
+        screenshot.getImage(),
+        PreviewController.IMAGE_FORMAT.toUpperCase(Locale.getDefault()),
+        getTargetFile()
+      );
+    } catch (IOException e) {
+      LOG.log(Level.WARNING, "Failed to write screenshot to file", e);
+    }
+  }
+
+  /** Returns file to save screenshot to. */
+  private File getTargetFile() {
     final File targetFile = new File(
-    previewsDir + previewID + "." + PreviewController.IMAGE_FORMAT
+            previewsDir + previewID + "." + PreviewController.IMAGE_FORMAT
     );
+
     if (!targetFile.getParentFile().exists()) {
       final boolean result = targetFile.getParentFile().mkdirs();
       if (!result) {
         LOG.warning("Failed to make dirs for preview directory");
       }
     }
+
     if (!targetFile.exists()) {
       try {
         final boolean result = targetFile.createNewFile();
@@ -120,17 +93,6 @@ public class GeneratePreviewThread extends Thread {
       }
     }
 
-    try {
-      ImageIO.write(
-        screenshot.getImage(),
-        PreviewController.IMAGE_FORMAT.toUpperCase(Locale.getDefault()),
-        targetFile
-      );
-    } catch (IOException e) {
-      LOG.warning("Failed to write screenshot to file: " + e.getMessage());
-      e.printStackTrace();
-    }
-    driver.close();
-    xvfbProcess.destroy();
+    return targetFile;
   }
 }
