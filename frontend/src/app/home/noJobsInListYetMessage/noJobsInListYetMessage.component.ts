31f887ce8e5847115cd3551cdcc257b1fc8d36b1
Nikita Volobuev <nikitavbv@gmail.com>
2019-08-07 00:18:01 +0300

    fix(noJobsInListMessage): fix html & style

--- frontend/src/app/home/noJobsInListYetMessage/noJobsInListYetMessage.component.ts
+++ frontend/src/app/home/noJobsInListYetMessage/noJobsInListYetMessage.component.ts
@@ -2,7 +2,8 @@ import {Component} from "@angular/core";
 
 @Component({
   selector: 'no-jobs-in-list-yet-message',
-  templateUrl: 'noJobsInListYetMessage'
+  templateUrl: 'noJobsInListYetMessage.component.html',
+  styleUrls: ['noJobsInListYetMessage.component.less']
 })
 export class NoJobsInListYetMessageComponent {
 }
